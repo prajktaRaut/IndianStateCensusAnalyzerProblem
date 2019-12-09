da50e312a487576b767196e1e1ac02fb061d17c4
prajktaRaut <prajktasraut16@gmail.com>
2019-12-09 11:48:30 +0530

    Added code to load State CSV file

--- CensusAnalyser/src/main/java/censusanalyser/CensusAnalyser.java
+++ CensusAnalyser/src/main/java/censusanalyser/CensusAnalyser.java
@@ -39,4 +39,29 @@ public class CensusAnalyser {
         }
 
     }
+
+    public int  loadIndiaStateCode(String csvFilePath) throws CensusAnalyserException {
+
+        try (
+                Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
+        ) {
+
+            CsvToBeanBuilder<IndiaStateCodeCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+            csvToBeanBuilder.withType(IndiaStateCodeCSV.class);
+            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+            CsvToBean<IndiaStateCodeCSV> csvToBean = csvToBeanBuilder.build();
+            Iterator<IndiaStateCodeCSV> censusCSVIterator = csvToBean.iterator();
+            Iterable<IndiaStateCodeCSV> csvIterable = () -> censusCSVIterator;
+
+            int namOfEateries = (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
+
+            return namOfEateries;
+
+        } catch (IOException e) {
+            throw new CensusAnalyserException(e.getMessage(),CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+        } catch (IllegalStateException e) {
+            throw new CensusAnalyserException(e.getMessage(),
+                    CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
+        }
+    }
 }
