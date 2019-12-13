a08eeb82e9abccdab48338553cd3d37592a21ca4
prajktaRaut <prajktasraut16@gmail.com>
2019-12-13 12:03:33 +0530

    Added test and code to load US Census Data using USCensusAdapter

--- CensusAnalyser/src/test/java/censusanalyser/USCensusAdapterTest.java
+++ CensusAnalyser/src/test/java/censusanalyser/USCensusAdapterTest.java
@@ -1,4 +1,73 @@
 package censusanalyser;
 
+import com.bridgelabzs.CSVBuilderException;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.util.Map;
+
 public class USCensusAdapterTest {
+
+    private static final String US_CENSUS_CSV_FILE_PATH = "./src/test/resources/USCensusData.csv";
+    private static final String CENSUS_FILE_FOR_WRONG_DELIMITER_OR_HEADER = "./src/test/resources/NewUSCensusData.csv";
+    private static final String WRONG_US_CENSUS_CSV_FILE_PATH = "./src/main/resources/USCensusData.csv";
+
+
+    USCensusAdapter usCensusAdapter=new USCensusAdapter();
+
+    @Test
+    public void givenUSCensusData_ShouldReturnCorrectRecords() {
+
+        try {
+            Map<String, IndiaCensusDAO> censusDAOMap = usCensusAdapter.loadCensusData(CensusAnalyser.Country.US, US_CENSUS_CSV_FILE_PATH);
+            Assert.assertEquals(51, censusDAOMap.size());
+        } catch (CSVBuilderException e) {
+        }
+    }
+
+    @Test
+    public void givenIndiaCensusData_WithWrongFile_ShouldThrowException() {
+        try {
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CSVBuilderException.class);
+            usCensusAdapter.loadCensusData(CensusAnalyser.Country.INDIA, WRONG_US_CENSUS_CSV_FILE_PATH);
+        } catch (CSVBuilderException e) {
+            Assert.assertEquals(CSVBuilderException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+        }
+    }
+
+    @Test
+    public void givenIndianCensusCSVFile_WithWrongFileType_ShouldThrowException() {
+        try {
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CSVBuilderException.class);
+            usCensusAdapter.loadCensusData(CensusAnalyser.Country.INDIA, WRONG_US_CENSUS_CSV_FILE_PATH);
+        } catch (CSVBuilderException e) {
+            Assert.assertEquals(CSVBuilderException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+        }
+    }
+
+    @Test
+    public void givenIndianCensusCSVFile_WithWrongDelimiterPosition_ShouldThrowException() {
+        try {
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CSVBuilderException.class);
+            usCensusAdapter.loadCensusData(CensusAnalyser.Country.INDIA, CENSUS_FILE_FOR_WRONG_DELIMITER_OR_HEADER);
+        } catch (CSVBuilderException e) {
+            Assert.assertEquals(CSVBuilderException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+        }
+    }
+
+    @Test
+    public void givenIndianCensusCSVFile_WithWrongHeader_ShouldThrowException() {
+        try {
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CSVBuilderException.class);
+            usCensusAdapter.loadCensusData(CensusAnalyser.Country.INDIA, CENSUS_FILE_FOR_WRONG_DELIMITER_OR_HEADER);
+        } catch (CSVBuilderException e) {
+            Assert.assertEquals(CSVBuilderException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+        }
+    }
+
 }
